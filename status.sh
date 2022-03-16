8d57a5026c74f9965409c878cd025c84040926d1
laoleic4 <56494512+bilibilimrlei@users.noreply.github.com>
2022-03-16 20:50:23 +0800

    Update status.sh

--- status.sh
+++ status.sh
@@ -84,8 +84,8 @@ check_region() {
 }
 Download_Server_Status_server() {
   cd "/tmp" || exit 1
-  [[ ${mirror_num} == 2 ]] && bundle_link="https://raw.iqiq.io/CokeMine/ServerStatus-Hotaru/archive/master.zip/" || bundle_link="https://github.com/CokeMine/ServerStatus-Hotaru/archive/master.zip"
-  [[ ${mirror_num} == 2 ]] && github_link="https://raw.iqiq.io/" || github_link="https://github.com"
+  [[ ${mirror_num} == 2 ]] && bundle_link="https://github.do/https://raw.githubusercontent.com/CokeMine/ServerStatus-Hotaru/archive/master.zip/" || bundle_link="https://github.com/CokeMine/ServerStatus-Hotaru/archive/master.zip"
+  [[ ${mirror_num} == 2 ]] && github_link="https://raw.iqiq.io" || github_link="https://github.com"
   wget -N --no-check-certificate "${bundle_link}" -O "master.zip"
   [[ ! -e "master.zip" ]] && echo -e "${Error} ServerStatus 服务端下载失败 !" && exit 1
   unzip master.zip
